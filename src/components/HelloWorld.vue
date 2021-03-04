fb71147f61aeeb3d484488ae805eacfab0bb37d3
Tom Afrialdi <tom.afrialdi@gmail.com>
2021-03-04 10:01:37 +0700

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,9 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      Tanya veronika asisten virtual adalah project chatbot yang dikembangkan oleh telkomsel,<br>
+      Untuk informasi selengkapnya kunjungi blog
+      <a href="https://kotakpintar.com/layanan-tanya-veronika-asisten-virtual-telkomsel/" target="_blank">Tanya Veronika Asisten Virtual</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
